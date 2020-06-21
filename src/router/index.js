19dc5e9af4fa4a9e60b65100a87ef7b60edab0ce
vijayshankar037 <vijayshankar1990@hotmail.com>
2020-06-21 12:18:46 +0530

    Route alias

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,14 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
+    path: "/about-us",
     name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    alias: "about"
   }
 ];
 
